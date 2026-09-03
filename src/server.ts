3e1352af72cdf8f937ac4591a83a3503bdd00c57
JP <khashkong@gmail.com>
2026-09-03 09:36:57 +0100

    feat(api): create express server

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.json({
+    message: "Card Validation API",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
